bcc59586d526f1e6bd427947c5c64253b1692dcf
Snehadri Bhattacharya <lostnfound7.snehadri@gmail.com>
2026-07-27 18:41:02 +0530

    Time: 20 ms (90%), Space: 47.8 MB (68%) - LeetHub

---
+++ 1410-html-entity-parser/1410-html-entity-parser.java
@@ -0,0 +1,10 @@
+class Solution {
+    public String entityParser(String text) {
+        return text.replace("&quot;", "\"")
+                   .replace("&apos;", "'")
+                   .replace("&gt;", ">")
+                   .replace("&lt;", "<")
+                   .replace("&frasl;", "/")
+                   .replace("&amp;", "&");
+    }
+}
